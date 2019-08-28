b332c7cda2e8a977d3efc4b948a8ea69ea5f6614
Miika Alikirri <miika.alik@gmail.com>
2019-08-29 00:03:07 +0300

    Added support for serving static files

--- src/http.c
+++ src/http.c
@@ -56,15 +56,51 @@
  */
 #include <unistd.h>
 
-// #include "crypto/base64.h"
-// #include "crypto/sha1.h"
-// #include "dataframe.h"
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
 #include "http.h"
 #include "options.h"
 #include "server.h"
 #include "socketcon.h"
 
 #define SERVER_STR "Server: webasmhttpd/0.0.1\r\n"
+
+static Filetype parse_filetype(const char* filepath)
+{
+    if (strncmp(filepath + (strlen(filepath)) - 3, ".js", 3) == 0)
+        return FILE_JS;
+    else if (strncmp(filepath + (strlen(filepath)) - 4, ".css", 4) == 0)
+        return FILE_CSS;
+    else if (strncmp(filepath + (strlen(filepath)) - 5, ".html", 5) == 0)
+        return FILE_HTML;
+
+    return FILE_DEFAULT;
+}
+
+static void send_filetype(Response* r, Filetype type)
+{
+    char buf[128];
+
+    switch (type) {
+    case FILE_HTML:
+        strcpy(buf, "Content-Type: text/html\r\n");
+        break;
+    case FILE_CSS:
+        strcpy(buf, "Content-Type: text/css\r\n");
+        break;
+    case FILE_JS:
+        strcpy(buf, "Content-Type: text/javascript\r\n");
+        break;
+    default:
+        // TODO: figure out better default content type
+        strcpy(buf, "Content-Type: text/html\r\n");
+        break;
+    }
+
+    send(r->conn.conn_fd, buf, strlen(buf), 0);
+}
 
 /**
  * @brief send 404 header to client
@@ -87,7 +123,7 @@ void http_404(Response* r)
     send(r->conn.conn_fd, buf, strlen(buf), 0);
 }
 
-void http_200(Response* r)
+void http_200(Response* r, Filetype type)
 {
     char buf[256];
 
@@ -95,8 +131,7 @@ void http_200(Response* r)
     send(r->conn.conn_fd, buf, strlen(buf), 0);
     strcpy(buf, SERVER_STR);
     send(r->conn.conn_fd, buf, strlen(buf), 0);
-    sprintf(buf, "Content-Type: text/html\r\n");
-    send(r->conn.conn_fd, buf, strlen(buf), 0);
+    send_filetype(r, type);
     strcpy(buf, "\r\n");
     send(r->conn.conn_fd, buf, strlen(buf), 0);
 }
@@ -118,6 +153,33 @@ void send_json(Response* r, JSONObject* obj)
     STRINGP_FREE(str);
 }
 
+void send_file(Response* r, const char* filepath)
+{
+    int fd;
+    // open the files relative to server
+    if (filepath[0] == '/')
+        fd = open(filepath + 1, O_RDONLY);
+    else
+        fd = open(filepath, O_RDONLY);
+    // File not found
+    if (fd == -1) {
+        http_404(r);
+        return;
+    }
+
+    struct stat st;
+    fstat(fd, &st);
+    char* file_content = ALLOCATE(char, st.st_size);
+    read(fd, file_content, st.st_size);
+
+    Filetype ftype = parse_filetype(filepath);
+    http_200(r, ftype);
+
+    send(r->conn.conn_fd, file_content, st.st_size, 0);
+    free(file_content);
+    close(fd);
+}
+
 void* accept_client(void* clientptr)
 {
     Connection conn;
@@ -132,8 +194,7 @@ void* accept_client(void* clientptr)
         parse_paramas(&r, au);
         (au->callback)(&resp, &r);
     } else {
-        //TODO: serve static file
-        http_404(&resp);
+        send_file(&resp, r.uri.chars);
     }
     if (_server_option_verbose_output)
         printf("request handled\n");
--- src/http.h
+++ src/http.h
@@ -4,9 +4,21 @@
 #include "requests/request.h"
 #include "utils/json.h"
 
+typedef enum {
+    FILE_DEFAULT,
+    FILE_HTML,
+    FILE_CSS,
+    FILE_JS
+} Filetype;
+
 void http_404(Response* resp);
-void http_200(Response* resp);
+void http_200(Response* r, Filetype type);
 void send_json(Response* resp, JSONObject* obj);
+/*
+* Send a file content basend on the filetype (.html, .css, .js etc)
+* Send 404 if file is not found
+*/
+void send_file(Response* r, const char* filepath);
 
 void* accept_client(void* clientptr);
 
