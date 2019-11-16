c86974fe7d1ec848e1ee093ecadca6326ce2ae84
Miika Alikirri <miika.alik@gmail.com>
2019-11-16 23:30:44 +0200

    Changed Makefile to cmake

--- run_tests.sh
+++ run_tests.sh
@@ -3,7 +3,6 @@
 # all|unit|integration defines what test are going to be run
 # if not assigned, it defaults to all
 RUN=$1
-echo $1
 if [[ $RUN != "unit" && $RUN != "integration" ]]
 then
     RUN="all"
@@ -12,9 +11,14 @@ fi
 # Fail as soon as a test fails
 set -e
 
+# Terminal colors
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
 function error() {
     echo ""
-    echo "Not all the tests passed"
+    printf "${RED}Not all the tests passed${NC}\n"
     kill -SIGTERM $U_PID &> /dev/null
 }
 trap error ERR
@@ -25,21 +29,24 @@ TESTS=(json server)
 I_TESTS=(staticfiles simpleapi)
 
 # Compile the library to make sure the changes are applied
-make NAME=restapitest -j`grep processor /proc/cpuinfo | wc -l` &> /dev/null
+mkdir -p build
+cd build
+cmake ..
+make -j4
+cd ..
 
 # Find the project objects so we can test them
-OBJECTS=build/restapitest.o
+OBJECTS=build/libcrestapi.a
 
 CC=gcc
 TEST_CFLAGS="-std=c99 -pthread -Wall -Wextra -Werror -Wno-unused-parameter -Wno-unused-function -lcheck -lsubunit -lrt -lm"
-UTEST_CFLAGS="-std=c99 -pthread -Wall -Wextra -Werror -Wno-unused-parameter -Wno-unused-function"
 
 if [[ $RUN == "unit" || $RUN == "all" ]]
 then
     echo "------------- Running unit tests -------------"
     for T in "${TESTS[@]}"
     do
-        $CC -o check_$T tests/check_$T.c $OBJECTS $TEST_CFLAGS &> /dev/null
+        $CC -o check_$T tests/check_$T.c $OBJECTS $TEST_CFLAGS
         ./check_$T
         rm check_$T
     done
@@ -52,7 +59,7 @@ then
     echo "------------- Running integration tests -------------"
     for U in "${I_TESTS[@]}"
     do
-        $CC -o i_$U tests/integration/i_$U.c $OBJECTS $TEST_CFLAGS &> /dev/null
+        $CC -o i_$U tests/integration/i_$U.c $OBJECTS $TEST_CFLAGS
         ./i_$U &> /dev/null &
         U_PID=$!
         python3 tests/integration/i_$U.py
@@ -63,5 +70,5 @@ then
     echo ""
 fi
 
-echo "All the tests passed"
+printf "${GREEN}All the tests passed${NC}\n"
 
